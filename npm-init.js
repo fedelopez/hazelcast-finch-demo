b10146c4dd1b0817fc1269f6379fd9db6a2974fe
Fede Lopez <fede.lopez@gmail.com>
2016-03-20 19:45:16 +1100

    Added "copia" lib to handle asset copying.

--- npm-init.js
+++ npm-init.js
@@ -1,13 +1,3 @@
-var fs = require('fs');
-
-var jsFolder = './src/main/resources/js';
-fs.createReadStream('./node_modules/codemirror/lib/codemirror.js').pipe(fs.createWriteStream(jsFolder + '/codemirror.js'));
-fs.createReadStream('./node_modules/codemirror/mode/javascript/javascript.js').pipe(fs.createWriteStream(jsFolder + '/javascript-mode.js'));
-fs.createReadStream('./node_modules/jquery/dist/jquery.min.js').pipe(fs.createWriteStream(jsFolder + '/jquery.min.js'));
-fs.createReadStream('./node_modules/rx/dist/rx.all.min.js').pipe(fs.createWriteStream(jsFolder + '/rx.all.min.js'));
-fs.createReadStream('./node_modules/rx-dom/dist/rx.dom.min.js').pipe(fs.createWriteStream(jsFolder + '/rx.dom.min.js'));
-
-var cssFolder = './src/main/resources/css';
-fs.createReadStream('./node_modules/codemirror/lib/codemirror.css').pipe(fs.createWriteStream(cssFolder + '/codemirror.css'));
-fs.createReadStream('./node_modules/codemirror/theme/dracula.css').pipe(fs.createWriteStream(cssFolder + '/dracula.css'));
-
+var copia = require('copia');
+var config = {"index-html": "./src/main/resources/index.html", "node-modules": "./node_modules"};
+copia.copia(config);
